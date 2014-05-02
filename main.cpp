1b24ba16d3c9882b7d2ee79004b8d5ac40789641
dev <cory@kreatednine.com>
2014-05-02 12:41:17 -0700

    cleaned up whitespace

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 
 extern "C" {
-	#include <lua.h>
-	#include <lauxlib.h> 
+    #include <lua.h>
+    #include <lauxlib.h> 
     #include <lualib.h>
 }
 
 int main(int argc, char** argv) {
 
-	std::string expr;
-	
-	if (argc == 2) {
-		expr = argv[1];
+    std::string expr;
 
-		// buils expression for lua
-		expr = "a = (" + expr + ")";
-		const char *cexpr = expr.c_str();
-		
-		lua_State* LS = luaL_newstate();
-		luaL_dostring(LS, cexpr);
-		lua_getglobal(LS, "a");
-		// return the result off the stack
-		int i = lua_tointeger(LS, -1);
+    if (argc == 2) {
+    expr = argv[1];
 
-		// output the answer
-		std::cout << " = " << i << "\n";
-		// close lua
-		lua_close(LS);
+        // buils expression for lua
+        expr = "a = (" + expr + ")";
+        const char *cexpr = expr.c_str();
 
-	} else {
-		std::cout << "Nothing to calculate";
-	}
+        lua_State* LS = luaL_newstate();
+        luaL_dostring(LS, cexpr);
+        lua_getglobal(LS, "a");
+        // return the result off the stack
+        int i = lua_tointeger(LS, -1);
 
-	return 0;
+        // output the answer
+        std::cout << " = " << i << "\n";
+        // close lua
+        lua_close(LS);
+
+    } else {
+        std::cout << "Nothing to calculate";
+    }
+    
+    return 0;
 }
